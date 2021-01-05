c8ddc4b0f082b3345d0b12bb02f790f5fd23a539
Ben Hayes <b.t.hayes@comcast.net>
2021-01-05 01:05:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,40 @@
+class CashRegister
+  
+  attr_accessor :total, :discount
+  
+  def initialize(discount = nil)
+    @total = 0 
+    @discount = discount
+    @items = []
+  end 
+  
+  def add_item(item, price, quantity = nil)
+    if quantity == nil 
+      @total = self.total + price
+      @items << item 
+    else
+      amount = price*quantity
+      @total = self.total + amount
+      @items << ([item] * quantity) 
+    end
+  end
+  
+  def apply_discount
+    if @discount != nil
+      discount_decimal = @discount/100.0
+      discount_amount = self.total*discount_decimal
+      @total = (self.total - discount_amount).to_i
+      "After the discount, the total comes to $#{@total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+  
+  def items
+    @items
+    
+  end
+    
 
+  
+end
